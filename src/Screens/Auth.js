44ed945bd4e000bf02ddbf178b79ab23da0e7d41
Nicholas Chao <nibchao@gmail.com>
2023-06-03 16:41:41 -0700

    added registering for account on mongodb

--- src/Screens/Auth.js
+++ src/Screens/Auth.js
@@ -2,74 +2,115 @@ import react from "react";
 import Form from "../Components/form.js";
 import { Button } from "@mui/material";
 
-class Auth extends react.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            showForm: false,
-            selectedForm: undefined,
+class Auth extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false,
+      selectedForm: undefined,
+    };
+  }
+
+  closeForm = () => {
+    this.setState({ showForm: false });
+  };
+
+  login = (data) => {
+    fetch(this.props.server_url + "/api/auth/login", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      res.json().then((data) => {
+        if (data.message === "Logged in") {
+          this.props.changeScreen("lobby");
+        } else {
+          alert(data.message);
+          // this.setState({screen: "auth"});
         }
-    }
+      });
+    });
+  };
 
-    closeForm = () => {
-        this.setState({showForm: false});
-    }
+  register = (data) => {
+    fetch(this.props.server_url + "/api/auth/register", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      res.json().then(() => {
+        console.log("nice");
+        this.props.changeScreen("auth");
+      });
+    });
+  };
 
-    login = (data) => {
-        fetch(this.props.server_url, + '/api/auth/login', {
-            method: "POST",
-            mode: 'cors',
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-                'Accept' : 'application/json'
-            },
-            body: JSON.stringify(data),
-        }).then((res) => {
-            res.json().then((data) => {
-                if (data.message === "Logged in"){
-                    this.props.changeScreen('lobby');
-                }
-                else{
-                    alert(data.message);
-                    // this.setState({screen: "auth"});
-                }
-            });
-        });
-    }
-
-    register = (data) => {
-        // TODO: write codes to register
-        console.log(data);
-        // look at week 7/8 video on how to register new account and add to mongodb database
-    }
-
-    render(){
-        let display = null;
-        if (this.state.showForm){
-            let fields = [];
-            if (this.state.selectedForm === "login"){
-                fields = ['username', 'password'];
-                display = <Form fields={fields} close={this.closeForm} type="login" submit={this.login} key={this.state.selectedForm}/>;
-            }
-            else if (this.state.selectedForm === "register"){
-                fields = [ 'username', 'password', 'name'];
-                display = <Form fields={fields} close={this.closeForm} type="register" submit={this.register} key={this.state.selectedForm}/>;
-            }   
-        }
-        else{
-            display = <div>
-                <Button onClick={() => this.setState({showForm: true, selectedForm:"login"})}> Login </Button>
-                <Button onClick={() => this.setState({showForm: true, selectedForm: "register"})}> Register </Button>
-                </div>              ;
-        }
-        return(
-            <div>
-                <h1> Welcome to our website! </h1>
-                {display}
-            </div>
+  render() {
+    let display = null;
+    if (this.state.showForm) {
+      let fields = [];
+      if (this.state.selectedForm === "login") {
+        fields = ["username", "password"];
+        display = (
+          <Form
+            fields={fields}
+            close={this.closeForm}
+            type="login"
+            submit={this.login}
+            key={this.state.selectedForm}
+          />
         );
+      } else if (this.state.selectedForm === "register") {
+        fields = ["username", "password", "name"];
+        display = (
+          <Form
+            fields={fields}
+            close={this.closeForm}
+            type="register"
+            submit={this.register}
+            key={this.state.selectedForm}
+          />
+        );
+      }
+    } else {
+      display = (
+        <div>
+          <Button
+            onClick={() =>
+              this.setState({ showForm: true, selectedForm: "login" })
+            }
+          >
+            {" "}
+            Login{" "}
+          </Button>
+          <Button
+            onClick={() =>
+              this.setState({ showForm: true, selectedForm: "register" })
+            }
+          >
+            {" "}
+            Register{" "}
+          </Button>
+        </div>
+      );
     }
+    return (
+      <div>
+        <h1> Welcome to our website! </h1>
+        {display}
+      </div>
+    );
+  }
 }
 
 export default Auth;
--- src/Screens/Lobby.js
+++ src/Screens/Lobby.js
@@ -1,42 +1,50 @@
 import react from "react";
 import { Button } from "@mui/material";
 
-class Lobby extends react.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            rooms: undefined,
-        }
-    }
+class Lobby extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: undefined,
+    };
+  }
 
-    componentDidMount(){
-        fetch(this.props.server_url + '/api/rooms/all', {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            }
-        }).then((res) => {
-            res.json().then((data) => {
-                this.setState({rooms:data});
-            });
-        });
-    }
+  componentDidMount() {
+    fetch(this.props.server_url + "/api/rooms/all", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      res.json().then((data) => {
+        this.setState({ rooms: data });
+      });
+    });
+  }
 
-
-    render(){
-        return(
-            <div>
-                <h1>Lobby</h1>
-                {this.state.rooms ? this.state.rooms.map((room) => {
-                    return <Button variant="contained" key={"roomKey"+room} onClick={() => alert(room)}>{room}</Button>
-                }) : "loading..."}
-                {/* write codes to join a new room using room id*/}
-                {/* write codes to enable user to create a new room*/}
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Lobby</h1>
+        {this.state.rooms
+          ? this.state.rooms.map((room) => {
+              return (
+                <Button
+                  variant="contained"
+                  key={"roomKey" + room}
+                  onClick={() => alert(room)}
+                >
+                  {room}
+                </Button>
+              );
+            })
+          : "loading..."}
+        {/* write codes to join a new room using room id*/}
+        {/* write codes to enable user to create a new room*/}
+      </div>
+    );
+  }
 }
 
 export default Lobby;
