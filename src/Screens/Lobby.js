8afd18bbadb204f6a8cfc973fe9d1d943e81987a
Nicholas Chao <nibchao@gmail.com>
2023-06-05 22:40:06 -0700

    added detecting room button clicks

--- src/Screens/Lobby.js
+++ src/Screens/Lobby.js
@@ -1,11 +1,20 @@
 import react from "react";
 import { Button } from "@mui/material";
 import Auth from "./Auth";
+import { io } from "socket.io-client";
 
 class Lobby extends react.Component {
   constructor(props) {
     super(props);
+    this.socket = io("http://localhost:3001", {
+      cors: {
+        origin: "http://localhost:3001",
+        credentials: true,
+      },
+      transports: ["websocket"],
+    });
     this.state = {
+      username: "",
       rooms: undefined,
     };
   }
@@ -27,20 +36,32 @@ class Lobby extends react.Component {
   render() {
     return (
       <div>
-        <Button variant="contained" onClick={() => {
-          fetch(this.props.server_url + "/api/auth/logout", {
-            method: "GET",
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }).then(() => {
-            return (
-            <div>
-              <Auth server_url={this.props.server_url} changeScreen="auth"/>
-            </div>)
-          }).then(() => window.location.reload())
-        }}> Logout </Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            fetch(this.props.server_url + "/api/auth/logout", {
+              method: "GET",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            })
+              .then(() => {
+                return (
+                  <div>
+                    <Auth
+                      server_url={this.props.server_url}
+                      changeScreen="auth"
+                    />
+                  </div>
+                );
+              })
+              .then(() => window.location.reload());
+          }}
+        >
+          {" "}
+          Logout{" "}
+        </Button>
         <h1>Lobby</h1>
         {this.state.rooms
           ? this.state.rooms.map((room) => {
@@ -48,7 +69,12 @@ class Lobby extends react.Component {
                 <Button
                   variant="contained"
                   key={"roomKey" + room}
-                  onClick={() => alert(room)}
+                  onClick={() =>
+                    this.socket.emit("join", {
+                      room: room,
+                      username: this.username, // this.username is not correct but i don't know how to get the logged in user's username
+                    })
+                  }
                 >
                   {room}
                 </Button>
