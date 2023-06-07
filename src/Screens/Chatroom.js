77a4d5280e08924d6a8ae689c0746669a211af2a
Nicholas Chao <nibchao@gmail.com>
2023-06-06 17:14:05 -0700

    temp commented out code + added keys to elements

--- src/Screens/Chatroom.js
+++ src/Screens/Chatroom.js
@@ -19,33 +19,34 @@ class Chatroom extends react.Component {
   }
 
   componentDidMount() {
-    console.log(this.props.roomID);
+    // console.log(this.props.roomID);
 
-    let data = {};
-    data["roomID"] = this.props.roomID;
+    // let data = {};
+    // data["roomID"] = this.props.roomID;
 
-    fetch(this.props.server_url + "/api/messages/all", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => {
-      res.json().then((data) => {
-        // probably supposed to do something, this was copied from screenhandler.js
-        //   if (data.message === "logged in") {
-        //     this.setState({ screen: "lobby" });
-        //   } else {
-        //     this.setState({ screen: "auth" });
-        //   }
-      });
-    });
+    // fetch(this.props.server_url + "/api/messages/all", {
+    //   method: "GET",
+    //   credentials: "include",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // }).then((res) => {
+    //   res.json().then((data) => {
+    //     // probably supposed to do something, this was copied from screenhandler.js
+    //     //   if (data.message === "logged in") {
+    //     //     this.setState({ screen: "lobby" });
+    //     //   } else {
+    //     //     this.setState({ screen: "auth" });
+    //     //   }
+    //   });
+    // });
   }
 
   sendMessage = (text) => {
     this.socket.emit("chat message", text);
     this.state.messages.push(text);
-    this.setState({ text: text });
+    this.setState({ messages: this.state.messages, text: text });
+    document.getElementById("text").value = '';
   };
 
   back = () => {
@@ -58,7 +59,7 @@ class Chatroom extends react.Component {
         Chatroom
         <ul>
           {this.state.messages.map((message) => (
-            <li>{message}</li>
+            <li key={"messageKey" + message}>{message}</li>
           ))}
         </ul>
         <input
